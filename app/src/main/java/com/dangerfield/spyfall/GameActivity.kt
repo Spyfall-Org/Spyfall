e09554bdfeddbfeb76f150d7cfb096073d3faf2c
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2019-04-09 09:11:30 -0500

    cleanes up game activity ui and implements end game button to delete firebase game node

--- app/src/main/java/com/dangerfield/spyfall/GameActivity.kt
+++ app/src/main/java/com/dangerfield/spyfall/GameActivity.kt
@@ -1,5 +1,6 @@
 package com.dangerfield.spyfall
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -13,6 +14,7 @@ import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
 import android.view.LayoutInflater
+import android.view.View
 
 import androidx.constraintlayout.widget.ConstraintLayout
 import kotlin.collections.HashMap
@@ -29,6 +31,13 @@ class GameActivity : AppCompatActivity() {
     val TAG = "Game Activity"
     var game: Game? = null
     var locations = ArrayList<String>()
+    var roles = ArrayList<String>()
+    //so if we dont want duplicate roles then after we assign one we would want to make it such
+    //that we cant assign that same one again. We need to find a way to set very players "role" field
+    //we need code somehwere that like pulls in all the roles and assigns the players roles on firebase
+    //randomly and deletes that indicy once done and then in the game people will just pull their role
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,9 +61,10 @@ class GameActivity : AppCompatActivity() {
                 // whenever data at this location is updated.
                 game = dataSnapshot.getValue(Game::class.java)
 
+                if(game!= null){
                 loadPlayers(game?.playerList!!)
                 startTimer(game?.timeLimit!!)
-                getLocationsFromFireBase()
+                getLocationsAndRolesFromFireBase()}
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -88,7 +98,48 @@ class GameActivity : AppCompatActivity() {
         }.start()
     }
 
+
+
+
+
+
+    fun getLocationsAndRolesFromFireBase(){
+        //TODO find out why this takes so long, may have something to do with async
+        //this method should load in all of the locations that the user chose
+        //option: game has a node of chosen packs and chosen location
+
+       Log.d(TAG,"trying to get pack: ${game?.locationPacks?.get(0)}")
+
+
+        val collectionRef = db.collection("${game?.locationPacks?.get(0)}")
+        collectionRef.get().addOnSuccessListener { documents ->
+            for (document in documents) {
+                Log.d("including location", document.id)
+                locations.add(document.id)
+                Log.d(TAG, "${document["roles"]}")
+                var instanceRoles = document["roles"] as ArrayList<String>
+                roles.addAll(instanceRoles)
+            }
+            Log.d("LOCATIONS","Locations = : $locations")
+            Log.d(TAG,"all roles: $roles")
+            tv_role.text = "You are a ${roles.get(Random().nextInt(roles.size))}"
+            //right now this makes a huge list of all locations but really we just need to pic a random
+            //location and THEN make a list of just those roles
+
+
+            //so this area is what gets called AFTER the data is loaded
+            loadLocationView()
+        }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
+
+
+    }
+
     fun loadPlayers(playerList: ArrayList<Player>){
+
+        //TODO right now the two main views load one after the other, can we do this asynchonously?
 
         var params = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
             TableRow.LayoutParams.WRAP_CONTENT)
@@ -98,42 +149,22 @@ class GameActivity : AppCompatActivity() {
             val row = TableRow(this).apply {
                 layoutParams = params
             }
-                for(j in 0..1) {
-                    val player_tv = LayoutInflater.from(this)
-                        .inflate(R.layout.game_player, row, false) as ConstraintLayout
-                    var tv = player_tv.getViewById(R.id.tv_in_game_player_name) as TextView
-                    if(i+j < playerList.size){
-                        tv.text = playerList[i + j].username
-                        row.addView(player_tv)
-                    }
-
+            for(j in 0..1) {
+                val player_tv = LayoutInflater.from(this)
+                    .inflate(R.layout.game_player, row, false) as ConstraintLayout
+                var tv = player_tv.getViewById(R.id.tv_in_game_player_name) as TextView
+                if(i+j < playerList.size){
+                    tv.text = playerList[i + j].username
+                    row.addView(player_tv)
                 }
-                  tbl_players.addView(row)
+
+            }
+            tbl_players.addView(row)
         }
 
     }
 
-
-
-
-    fun getLocationsFromFireBase(){
-        //this method should load in all of the locations that the user chose
-        //option: game has a node of chosen packs and chosen location
-
-       Log.d(TAG,"trying to get pack: ${game?.locationPacks?.get(0)}")
-
-        val collectionRef = db.collection("${game?.locationPacks?.get(0)}")
-        collectionRef.get().addOnSuccessListener { documents ->
-            for (document in documents) {
-                Log.d("including location", document.id)
-                locations.add(document.id)
-            }
-        }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents: ", exception)
-            }
-
-
+    fun loadLocationView(){
         var params = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
             TableRow.LayoutParams.WRAP_CONTENT)
         params.setMargins(10,10,10,10)
@@ -155,9 +186,21 @@ class GameActivity : AppCompatActivity() {
             tbl_locations.addView(row)
         }
 
+    }
 
+    fun endGame(view: View){
+        //called when end button game is clicked
+        //deleted node on firebase
+
+        val ref = FirebaseDatabase.getInstance().getReference("/games/$ACCESS_CODE")
+        ref.removeValue()
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
 
 
     }
+
+
 
 }
