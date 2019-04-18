a46babc6490fec19a08e06f33c692d98d54fc478
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2019-04-17 22:00:10 -0500

    makes access codes lowercase

--- app/src/main/java/com/dangerfield/spyfall/NewGameActivity.kt
+++ app/src/main/java/com/dangerfield/spyfall/NewGameActivity.kt
@@ -80,7 +80,7 @@ class NewGameActivity : AppCompatActivity() {
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
-    fun generateAccessCode() = UUID.randomUUID().toString().substring(0,6).toUpperCase()
+    fun generateAccessCode() = UUID.randomUUID().toString().substring(0,6).toLowerCase()
 
 
 //TODO: we coudl possibly get the chosenLocation and roles here and never have to store the chosenPacks on firebase
