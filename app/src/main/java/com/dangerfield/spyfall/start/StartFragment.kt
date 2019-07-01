eef02d17d2e9057675cb5caa8c9650fdd2fdcff5
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2019-06-30 23:08:30 -0500

    asks users for review every 5 games

--- app/src/main/java/com/dangerfield/spyfall/start/StartFragment.kt
+++ app/src/main/java/com/dangerfield/spyfall/start/StartFragment.kt
@@ -1,5 +1,6 @@
 package com.dangerfield.spyfall.start
 
+import android.content.Context
 import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -51,6 +52,16 @@ class StartFragment : Fragment() {
         super.onResume()
         UIHelper.getSavedColor(context!!)
         changeAccent()
+
+        //increment number of games played, if it is a multiple of 5 and the user hasnt revied, ask for a review
+        if(incrementGamePlay() % 5 == 0 && !userHasReviewed()){
+            //show request for review
+            UIHelper.customSimpleAlert(context!!,"Like the game?","Pretty please let us know by giving us a review :)",
+                "Okay",{
+                    //TODO: bring user to app store
+                    setUserReview()
+                },"NO!",{}).show()
+        }
     }
 
     fun changeAccent(){
@@ -60,5 +71,35 @@ class StartFragment : Fragment() {
         drawable.setColorFilter(UIHelper.accentColor, PorterDuff.Mode.SRC_ATOP)
         btn_rules.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
         btn_rules.setTextColor(UIHelper.accentColor)
+    }
+
+    private fun incrementGamePlay(): Int{
+        val editor = context!!.getSharedPreferences(resources.getString(R.string.shared_preferences),
+            Context.MODE_PRIVATE
+        ).edit()
+
+        val newVal = getGamesPlayed() + 1
+        editor.putInt(resources.getString(R.string.shared_preferences_games), newVal)
+        editor.apply()
+
+        return newVal
+    }
+
+    private fun setUserReview(){
+        val editor = context!!.getSharedPreferences(resources.getString(R.string.shared_preferences),
+            Context.MODE_PRIVATE
+        ).edit()
+        editor.putBoolean(resources.getString(R.string.shared_preferences_hasReviewed), true)
+        editor.apply()
+    }
+
+    fun getGamesPlayed(): Int{
+        val prefs = context!!.getSharedPreferences(context!!.resources.getString(R.string.shared_preferences), Context.MODE_PRIVATE)
+        return prefs.getInt(context!!.resources.getString(R.string.shared_preferences_games), 0)
+    }
+
+    fun userHasReviewed(): Boolean {
+        val prefs = context!!.getSharedPreferences(context!!.resources.getString(R.string.shared_preferences), Context.MODE_PRIVATE)
+        return prefs.getBoolean(context!!.resources.getString(R.string.shared_preferences_hasReviewed), false)
     }
 }
