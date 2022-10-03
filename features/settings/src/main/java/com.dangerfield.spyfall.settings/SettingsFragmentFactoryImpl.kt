9e647ee3866f3ea2618a0d32c3fe444d33f82dec
Eli Dangerfield <eli.dangerfield@hulu.com>
2022-10-02 19:00:41 -0500

    removes and uneeded class

--- features/settings/src/main/java/com.dangerfield.spyfall.settings/SettingsFragmentFactoryImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.dangerfield.spyfall.settings
-
-import androidx.fragment.app.Fragment
-import com.dangerfield.spyfall.settingsapi.SettingsFragmentFactory
-
-class SettingsFragmentFactoryImpl : SettingsFragmentFactory {
-    override fun get(): Fragment = SettingsFragment.get()
-}
