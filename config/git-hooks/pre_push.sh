b366f4fef5552df379303b41ed9b8de7e621a1a9
Eli Dangerfield <eli.dangerfield@disneystreaming.com>
2022-12-20 22:27:10 -0600

    new pre-push hook

--- config/git-hooks/pre_push.sh
+++ config/git-hooks/pre_push.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 # run code analysis
+echo "*****Running Code Analysis******"
+
+git stash -q --keep-index
+
 ./gradlew detektAll
 ./gradlew checkstyleAll
+
+status=$?
+
+echo "*****Done with Code Analysis******"
+
+exit $status
+
