0fb2fa166a4bfc71a6bf2b4595daf1fe52c103c0
Eli Dangerfield <eli.dangerfield@disneystreaming.com>
2022-12-20 22:10:07 -0600

    ed/SPYF-60 adds correct file name for pre-push

--- build-logic/convention/src/main/java/com/spyfall/convention/shared/GitHooksCheck.kt
+++ build-logic/convention/src/main/java/com/spyfall/convention/shared/GitHooksCheck.kt
@@ -1,6 +1,5 @@
 package com.spyfall.convention.shared
 
-import com.spyfall.convention.shared.task.printGreen
 import org.gradle.api.Project
 
 internal fun Project.configureGitHooksCheck() {
@@ -9,7 +8,6 @@ internal fun Project.configureGitHooksCheck() {
         project.rootProject.file("config/git-hooks")
             .listFiles()
             .forEach {
-                printGreen("Searching for ${it.name} in the .git/hooks folder")
                 val installedGitHook = project.rootProject.file(".git/hooks/${it.name}")
 
                 if (!installedGitHook.isFile && !BuildEnvironment.isCIBuild) {
