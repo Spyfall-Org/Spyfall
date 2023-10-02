778097d7bb45f1dc8097f415bc41be5c81ab34ec
elijah-dangerfield <elijahdangerfield111@gmail.com>
2023-10-02 14:54:56 -0400

    updates release branch validation script

--- scripts/ci/validate_release_inputs.main.kts
+++ scripts/ci/validate_release_inputs.main.kts
@@ -12,27 +12,21 @@ fun printGreen(text: String) {
     println(green + text + reset)
 }
 
-if (args.size < 2 || args[0] == "-h" || args[0].contains("help")) {
+if (args.isEmpty() || args[0] == "-h" || args[0].contains("help")) {
     printRed("""
-        This script validates the inputs to the create_release_branch.yml scrtipt 
+        This script validates the inputs to the create_release_branch.yml script 
         
-        Usage: ./validate_release_inputs.main.kts  [versionName] [versionCode]
+        Usage: ./validate_release_inputs.main.kts  [versionName]
         versionName (required): "1.2.4", "1.5.19", ...
-        versionCode (optional): "500", "15", ...
     """.trimIndent())
 
     @Suppress("TooGenericExceptionThrown")
     throw Exception("See Message Above")
 }
 
-
-val appName = args[0]
-val appVersion = args[1]
-val versionCode = args.getOrNull(2)
+val appVersion = args[0]
 
 class ValidationError(validationErrorMessage: String) :Exception(validationErrorMessage)
-
-fun isVersionCodeValid() = versionCode == null || versionCode.toIntOrNull() != null && versionCode.toInt() > 0
 
 fun isAppVersionValid(): Boolean {
     // Regular expression to match a version code of the form "x.y.z"
@@ -48,14 +42,8 @@ val appVersionValidationError = ValidationError("""
     Please make sure you enter an app version in the format x.y.z (major, minor, patch [optional])
 """.trimIndent())
 
-val versionCodeValidationError = ValidationError("""
-    The version code "$versionCode" is invalid. 
-    Please make sure you enter a numeric app version code
-""".trimIndent())
-
 
 when {
     !isAppVersionValid() -> throw appVersionValidationError
-    !isVersionCodeValid() -> throw versionCodeValidationError
     else -> printGreen("Good news. The inputs to create the release are valid. ")
 }
