ccff78b9990dc578605955756e00dc5cad66ec2d
elijah-dangerfield <elijahdangerfield111@gmail.com>
2023-10-05 10:52:49 -0400

    updates deploy script

--- build-logic/convention/src/main/java/com/spyfall/convention/util/AppModuleSecretFilesCheck.kt
+++ build-logic/convention/src/main/java/com/spyfall/convention/util/AppModuleSecretFilesCheck.kt
@@ -1,6 +1,7 @@
 package com.spyfall.convention.util
 
 import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.utils.`is`
 import java.io.File
 import java.lang.System.getenv
 
@@ -14,25 +15,32 @@ internal fun Project.checkForAppModuleSecretFiles() {
     fun isAppMissingSecretFiles(): Boolean = secretFiledNeeded.any { !it.isFile }
 
     fun installSecretFiles() {
-        val serviceKeyPath = getenv("SPYFALL_SERVICE_KEY_PATH") ?: "${project.rootDir}/service_key.json"
-        if (!File(serviceKeyPath).isFile) {
 
-            printRed(
+        val serviceKeyPath = getenv("SPYFALL_SERVICE_KEY_PATH")
+            ?: "${project.rootDir}/service_key.json".takeIf { File(it).isFile }
+            ?: "${project.rootDir}/app/service_key.json".takeIf { File(it).isFile }
+
+        if (serviceKeyPath == null || !File(serviceKeyPath).isFile) {
+            @Suppress("UseCheckOrError")
+            throw IllegalStateException(
                 """
+                   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+                    
                     Could not find the service_key.json needed to download secret files from google drive. 
                     Please download the service_key.json here: 
                     https://drive.google.com/file/d/1t456fo07BN9NF0a3e1Ds9KNBccV1X1AQ/view?usp=share_link
                     
-                    Once the file is saved, copy the path and add the following line to your ~/.bashrc 
+                    Once the file is saved, 
+                    you can either
+                    1. move the file to the `app` folder
+                    
+                    2. copy the path and add the following line to your ~/.bashrc 
                     or ~/.zshrc (depending on your set up)
+                    `export SPYFALL_SERVICE_KEY_PATH="INSERT_PATH"`
                     
-                    export SPYFALL_SERVICE_KEY_PATH="INSERT_PATH"
-                    
+                    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
                 """.trimIndent()
-            )
-            @Suppress("UseCheckOrError")
-            throw IllegalStateException(
-                "Could not find the service_key.json needed to download secret files from google drive."
             )
         } else {
             val result = ProcessBuilder("./scripts/get_secret_files.main.kts", serviceKeyPath)
@@ -43,12 +51,13 @@ internal fun Project.checkForAppModuleSecretFiles() {
 
             @Suppress("TooGenericExceptionThrown")
             if (result != 0) {
-                printRed("""
+                @Suppress("TooGenericExceptionThrown")
+                throw Exception(
+                    """
                    Failed to run ./scripts/get_secret_files.main.kts with input "$serviceKeyPath"
                    Please see AppModuleSecretFilesCheck.kt
-                """.trimIndent())
-                @Suppress("TooGenericExceptionThrown")
-                throw Exception("See Message Above")
+                """.trimIndent()
+                )
             } else {
                 printGreen("Finished downloading all secret files")
             }
@@ -56,10 +65,12 @@ internal fun Project.checkForAppModuleSecretFiles() {
     }
 
     if (isAppMissingSecretFiles()) {
-        printRed("MISSING SECRET FILES FOR PROJECT ${project.name.toUpperCase()}. " +
-                "\nUPDATING ALL SECRET FILES.")
+        printRed(
+            "MISSING SECRET FILES FOR PROJECT ${project.name.toUpperCase()}. " +
+                    "\nUPDATING ALL SECRET FILES."
+        )
         installSecretFiles()
     } else {
-        printGreen("Project ${project.name } has all Secret files needed.")
+        printGreen("Project ${project.name} has all Secret files needed.")
     }
 }
