6ca519b20ea383f9bbe09055a0256917488d6ce7
elijah-dangerfield <elijahdangerfield111@gmail.com>
2024-01-15 10:36:31 -0500

    adds stats

--- libraries/storage/internal/src/main/java/com/dangerfield/libraries/storage/internal/AppDatabaseModule.kt
+++ libraries/storage/internal/src/main/java/com/dangerfield/libraries/storage/internal/AppDatabaseModule.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DatabaseModule {
+object AppDatabaseModule {
     @Provides
     @Singleton
     fun provideDatabase(
