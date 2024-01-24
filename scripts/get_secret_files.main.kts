6583c6a6da7e4004e9f69ba15270830330bc5321
elijah-dangerfield <elijahdangerfield111@gmail.com>
2024-01-24 16:44:13 -0500

    updates ci scripts

--- scripts/get_secret_files.main.kts
+++ scripts/get_secret_files.main.kts
@@ -14,48 +14,54 @@ import com.google.auth.http.HttpCredentialsAdapter
 import com.google.auth.oauth2.ServiceAccountCredentials
 import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.FileInputStream
 import java.io.FileOutputStream
 
 // These file ids can be found in the url of the sharable google drive links to these files
-data class FileInfo(val id: String, val pathToStore: String)
+data class FileInfo(val id: String, val pathsToStore: List<String>)
 
 // TODO debug builds dont need the release stuff. Same for the service account keys.
 // Neither of these are used in day to day. Seems odd to require them to be installed.
 
 // Used to talk to the release firebase project
-val spyfallReleaseGoogleServicesFileInfo =
+val releaseGoogleServicesFileInfo =
     FileInfo(
-        id ="1UIP-nsDLazFCo-OIU3OzFjiBNTeyAjRc",
-        pathToStore = "app/src/release/google-services.json"
-    )
-
-// Used to talk to the debug firebase project
-val spyfallDebugGoogleServicesFileInfo =
-    FileInfo(
-        id ="12rbE9BCp8UkQ38_eE465tjI8O3VbYAzL",
-        pathToStore = "app/src/debug/google-services.json"
-    )
-
-// Used to make changes to the debug firestore project via scripts
-val spyfallDebugServiceAccountKeyFileInfo =
-    FileInfo(
-        id ="1wEDOUOW3-ULVAyWe3NRmIiF5s3K_E6Lq",
-        pathToStore = "app/src/debug/service-account-key.json"
+        id = "1UIP-nsDLazFCo-OIU3OzFjiBNTeyAjRc",
+        pathsToStore = listOf(
+            "app/src/release/google-services.json",
+            "app/src/qa/google-services.json",
+        )
     )
 
 // Used to make changes to the release firestore project via scripts
-val spyfallReleaseServiceAccountKeyFileInfo =
+val releaseServiceAccountKeyFileInfo =
     FileInfo(
-        id ="13Q-z85mO3-5JVFhXmQ9191-N465-18PH",
-        pathToStore = "app/src/release/service-account-key.json"
+        id = "13Q-z85mO3-5JVFhXmQ9191-N465-18PH",
+        pathsToStore = listOf(
+            "app/src/release/service-account-key.json",
+            "app/src/qa/service-account-key.json",
+        )
     )
 
+// Used to talk to the debug firebase project
+val debugGoogleServicesFileInfo =
+    FileInfo(
+        id = "12rbE9BCp8UkQ38_eE465tjI8O3VbYAzL",
+        pathsToStore = listOf("app/src/debug/google-services.json")
+    )
+
+// Used to make changes to the debug firestore project via scripts
+val debugServiceAccountKeyFileInfo =
+    FileInfo(
+        id = "1wEDOUOW3-ULVAyWe3NRmIiF5s3K_E6Lq",
+        pathsToStore = listOf("app/src/debug/service-account-key.json")
+    )
+
+
 val fileInfoList = listOf(
-    spyfallReleaseGoogleServicesFileInfo,
-    spyfallReleaseServiceAccountKeyFileInfo,
-    spyfallDebugGoogleServicesFileInfo,
-    spyfallDebugServiceAccountKeyFileInfo,
+    releaseGoogleServicesFileInfo,
+    releaseServiceAccountKeyFileInfo,
+    debugGoogleServicesFileInfo,
+    debugServiceAccountKeyFileInfo,
 )
 
 val red = "\u001b[31m"
@@ -71,8 +77,9 @@ fun printGreen(text: String) {
 }
 
 val isHelpCall = args.isNotEmpty() && (args[0] == "-h" || args[0].contains("help"))
-if ( isHelpCall ) {
-    printRed("""
+if (isHelpCall) {
+    printRed(
+        """
         This script collects every required files to run the debug and release variants
         from google drive using a service account key file. You must either pass in the path to the service_key.json
         file or have it installed in the root directory. 
@@ -81,28 +88,31 @@ if ( isHelpCall ) {
         https://drive.google.com/file/d/1t456fo07BN9NF0a3e1Ds9KNBccV1X1AQ/view?usp=share_link
         
         Usage: ./create_google_json_files.main.kts [optional serviceAccountKeyPath]
-    """.trimIndent())
+    """.trimIndent()
+    )
 
     @Suppress("TooGenericExceptionThrown")
     throw Exception("See Message Above")
 }
 
 val serviceAccountKeyPath = args.getOrNull(0) ?: run {
-    if ( File("service_key.json").isFile) return@run "service_key.json"
-    printRed("""
+    if (File("service_key.json").isFile) return@run "service_key.json"
+    printRed(
+        """
         You must either pass in the path to the service_key.json
         file or have it installed in the root directory. 
         
         You can download the service_key.json file from the following link: 
         https://drive.google.com/file/d/1t456fo07BN9NF0a3e1Ds9KNBccV1X1AQ/view?usp=share_link
-    """.trimIndent())
+    """.trimIndent()
+    )
     @Suppress("TooGenericExceptionThrown")
     throw Exception("No service_key.json file found")
 }
 
 fun getFiles() {
-    val file = File(serviceAccountKeyPath)
-    val credentials = ServiceAccountCredentials.fromStream(file.inputStream())
+    val googleDriveServiceAccountKey = File(serviceAccountKeyPath)
+    val credentials = ServiceAccountCredentials.fromStream(googleDriveServiceAccountKey.inputStream())
     val scopedCredentials = credentials.createScoped(listOf(DriveScopes.DRIVE))
 
     val transport: HttpTransport = NetHttpTransport()
@@ -118,19 +128,35 @@ fun getFiles() {
 
     var successfulFetches = 0
     fileInfoList.forEach {
-        val driveFile = drive.files().get(it.id).execute()
+        try {
+            it.pathsToStore.forEach { path ->
+                val file = File(path)
+                if (!file.isFile) {
+                    file.createNewFile()
+                }
+            }
 
-        if (driveFile.mimeType == "application/json") {
-            val outputStream = ByteArrayOutputStream()
-            drive.files().get(it.id).executeMediaAndDownloadTo(outputStream)
-            val outputFile = FileOutputStream(it.pathToStore)
-            outputStream.writeTo(outputFile)
+            val driveFile = drive.files().get(it.id).execute()
+
+            if (driveFile.mimeType == "application/json") {
+                val outputStream = ByteArrayOutputStream()
+                drive.files().get(it.id).executeMediaAndDownloadTo(outputStream)
+                val outputFiles = it.pathsToStore.map { path ->
+                    FileOutputStream(path)
+                }
+
+                outputFiles.forEach {
+                    outputStream.writeTo(it)
+                }
+            }
+
+            successfulFetches += 1
+        } catch (t: Throwable) {
+            printRed(t.message ?: "Error getting file ${it.pathsToStore}")
         }
-
-        successfulFetches += 1
     }
 
-    if(successfulFetches == fileInfoList.size) {
+    if (successfulFetches == fileInfoList.size) {
         printGreen("Downloaded all secret files")
     } else {
         printRed("DID NOT Download all secret files")
