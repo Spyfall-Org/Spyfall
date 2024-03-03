80df7116ab358b3017a52884d834a119ed6c7f3b
elijah-dangerfield <elijahdangerfield111@gmail.com>
2024-03-02 18:24:38 -0600

    adds color swatches

--- libraries/ui/src/main/java/com/dangerfield/libraries/ui/color/ColorResource.kt
+++ libraries/ui/src/main/java/com/dangerfield/libraries/ui/color/ColorResource.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -32,58 +33,47 @@ import com.dangerfield.libraries.ui.Radii
 import com.dangerfield.libraries.ui.VerticalSpacerD100
 import com.dangerfield.libraries.ui.components.text.Text
 import com.dangerfield.libraries.ui.theme.OddOneOutTheme
+import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.primaryConstructor
 
 @Suppress("ClassNaming")
+@Stable
 sealed class ColorResource(val color: Color, val designSystemName: String) {
-    data object Unspecified: ColorResource(Color.Unspecified, "color-unspecified")
-    data object Black900: ColorResource(Color(0xFF000000), "color-black-900")
-    data object Black900_A_70: ColorResource(Color(0xFF000000).copy(alpha = 0.7f), "color-black-900-a-70")
-    data object Black800: ColorResource(Color(0xFF1A1A1A), "color-black-800")
-    data object Black700: ColorResource(Color(0xFF313131), "color-black-700")
-    data object Black600: ColorResource(Color(0xFF767676), "color-black-600")
-    data object Black500: ColorResource(Color(0xFFAAAAAA), "color-black-500")
-    data object Black400: ColorResource(Color(0xFFCCCCCC), "color-black-400")
-    data object Black300: ColorResource(Color(0xFFEEEEEE), "color-black-300")
-    data object Black200: ColorResource(Color(0xFFF6F6F6), "color-black-200")
-    data object Black100: ColorResource(Color(0xFFFBFBFB),  "color-black-100")
-    data object Purple900: ColorResource(Color(0xFF270B68), "color-purple-900")
-    data object Purple700: ColorResource(Color(0xFF422091), "color-purple-700")
-    data object Purple500: ColorResource(Color(0xFF4C27A3), "color-purple-500")
-    data object Purple400: ColorResource(Color(0xFF5B35B4), "color-purple-400")
-    data object Purple300: ColorResource(Color(0xFF784CDF), "color-purple-300")
-    data object Purple100: ColorResource(Color(0xFFA179FF), "color-purple-100")
-    data object PurpleGray300: ColorResource(Color(0xFF8C79B8), "color-purple-300")
-    data object White900: ColorResource(Color(0xFFFFFFFF), "color-white-900")
-    data object GrapeJelly500: ColorResource(Color(0xFF9533C7), "color-grape-jelly-500")
-    data object SkyDive400: ColorResource(Color(0xFF00A0EF), "color-sky-dive-400")
-    data object MintyFresh300: ColorResource(Color(0xFF2FD566), "color-minty-fresh-300")
-    data object TangerineTwist600: ColorResource(Color(0xFFFF5800), "color-tangerine-twist-600")
-    data object CherryPop700: ColorResource(Color(0xFFE3212F), "color-cherry-pop-700")
-    internal class FromColor(color: Color, name: String): ColorResource(color, name)
+    object Unspecified : ColorResource(Color.Unspecified, "unspecified")
+    object Black900 : ColorResource(Color(0xFF000000), "black-900")
+    object Black900_A_70 :
+        ColorResource(Color(0xFF000000).copy(alpha = 0.7f), "black-900-a-70")
 
-    fun copyWith(alpha: Float): ColorResource {
-        return FromColor(color.copy(alpha = alpha), this.designSystemName)
-    }
+    object Black800 : ColorResource(Color(0xFF1A1A1A), "black-800")
+    object Black700 : ColorResource(Color(0xFF313131), "black-700")
+    object Black600 : ColorResource(Color(0xFF767676), "black-600")
+    object Black500 : ColorResource(Color(0xFFAAAAAA), "black-500")
+    object Black400 : ColorResource(Color(0xFFCCCCCC), "black-400")
+    object Black300 : ColorResource(Color(0xFFEEEEEE), "black-300")
+    object Black200 : ColorResource(Color(0xFFF6F6F6), "black-200")
+    object Black100 : ColorResource(Color(0xFFFBFBFB), "black-100")
+    object Purple900 : ColorResource(Color(0xFF270B68), "purple-900")
+    object Purple700 : ColorResource(Color(0xFF422091), "purple-700")
+    object Purple500 : ColorResource(Color(0xFF4C27A3), "purple-500")
+    object Purple400 : ColorResource(Color(0xFF5B35B4), "purple-400")
+    object Purple300 : ColorResource(Color(0xFF784CDF), "purple-300")
+    object Purple100 : ColorResource(Color(0xFFA179FF), "purple-100")
+    object PurpleGray300 : ColorResource(Color(0xFF8C79B8), "purple-300")
+    object White900 : ColorResource(Color(0xFFFFFFFF), "white-900")
+    object GrapeJelly500 : ColorResource(Color(0xFF9533C7), "grape-jelly-500")
+    object SkyDive400 : ColorResource(Color(0xFF00A0EF), "sky-dive-400")
+    object MintyFresh300 : ColorResource(Color(0xFF2FD566), "minty-fresh-300")
+    object TangerineTwist600 : ColorResource(Color(0xFFFF5800), "tangerine-twist-600")
+    object CherryPop700 : ColorResource(Color(0xFFE3212F), "cherry-pop-700")
+    internal class FromColor(color: Color, name: String) : ColorResource(color, name)
 
     val onColorResource: ColorResource
         get() {
             return if (color.luminance() > 0.4) Black900 else White900
         }
-
 }
-val ColorResource.name: String
-    get() = this.toString().lowercase()
-        .replace(Regex("""([A-Za-z]+)(\d+)"""), """color-$1-$2""")
-
-sealed class ColorGradientResource(val from: ColorResource, val to: ColorResource) {
-    object Dark: ColorGradientResource(ColorResource.Black900, ColorResource.Black800)
-    object Light: ColorGradientResource(ColorResource.Black100, ColorResource.Black200)
-    class FromTo(from: ColorResource, to: ColorResource): ColorGradientResource(from, to)
-    val colorOn: Color = from.onColorResource.color
-}
-
-val ColorGradientResource.name: String
-    get() = "color-gradient-${this.javaClass.simpleName.lowercase()}"
 
 @Composable
 fun animateColorResourceAsState(
@@ -94,17 +84,14 @@ fun animateColorResourceAsState(
 ): State<ColorResource> {
     val converter: TwoWayConverter<ColorResource, AnimationVector4D> = remember(targetValue) {
         val colorConverter = (Color.VectorConverter)(targetValue.color.colorSpace)
-        TwoWayConverter(
-            convertToVector = { token: ColorResource ->
-                colorConverter.convertToVector(token.color)
-            },
-            convertFromVector = { vector ->
-                ColorResource.FromColor(
-                    color = colorConverter.convertFromVector(vector),
-                    name = targetValue.designSystemName
-                )
-            }
-        )
+        TwoWayConverter(convertToVector = { token: ColorResource ->
+            colorConverter.convertToVector(token.color)
+        }, convertFromVector = { vector ->
+            ColorResource.FromColor(
+                color = colorConverter.convertFromVector(vector),
+                name = targetValue.designSystemName
+            )
+        })
     }
 
     return animateValueAsState(
@@ -116,19 +103,40 @@ fun animateColorResourceAsState(
     )
 }
 
+private val colors = listOf(
+    ColorResource.Black900,
+    ColorResource.Black900_A_70,
+    ColorResource.Black800,
+    ColorResource.Black700,
+    ColorResource.Black600,
+    ColorResource.Black500,
+    ColorResource.Black400,
+    ColorResource.Black300,
+    ColorResource.Black200,
+    ColorResource.Black100,
+    ColorResource.Purple900,
+    ColorResource.Purple700,
+    ColorResource.Purple500,
+    ColorResource.Purple400,
+    ColorResource.Purple300,
+    ColorResource.Purple100,
+    ColorResource.PurpleGray300,
+    ColorResource.White900,
+    ColorResource.GrapeJelly500,
+    ColorResource.SkyDive400,
+    ColorResource.MintyFresh300,
+    ColorResource.TangerineTwist600,
+    ColorResource.CherryPop700,
+)
 
-@Preview(widthDp = 360, heightDp = 640, showBackground = true)
+@Preview(widthDp = 400, heightDp = 1500, showBackground = false)
 @Composable
 private fun PreviewColorSwatch() {
-    val colorResources = remember {
-        sealedValues<ColorResource>()
-    }
-
-    Preview {
-
-        Text(text = "Number: ${colorResources.size}")
-        LazyVerticalGrid(columns = GridCells.Fixed(3)) {
-            items(colorResources) { colorResource ->
+    Preview(showBackground = false) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3)
+        ) {
+            items(colors) { colorResource ->
                 ColorCard(colorResource)
             }
         }
@@ -138,14 +146,14 @@ private fun PreviewColorSwatch() {
 
 @Composable
 internal fun ColorCard(colorResource: ColorResource) {
-    Box(modifier = Modifier
-        .background(colorResource.color, shape = Radii.Card.shape)
-        .height(150.dp)
-        .width(120.dp)
-        .clip(Radii.Card.shape)
-    )
-
-    {
+    Box(
+        modifier = Modifier
+            .padding(Dimension.D100)
+            .background(colorResource.color, shape = Radii.Card.shape)
+            .height(150.dp)
+            .width(120.dp)
+            .clip(Radii.Card.shape)
+    ) {
         Column(
             Modifier
                 .fillMaxWidth()
@@ -187,8 +195,3 @@ internal fun ColorResource.toHexString(includeAlpha: Boolean = true): String {
     val blue = "%02X".format((color.blue * 255).toInt())
     return "#$alpha$red$green$blue"
 }
-
-inline fun <reified T> sealedValues(): List<T> {
-    return T::class.sealedSubclasses.mapNotNull { it.objectInstance as T }
-}
-
